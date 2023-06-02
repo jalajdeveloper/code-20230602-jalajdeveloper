3063bd01a478f6d0219f32d861ae16f5f5746850
jalajdeveloper <jalajdeveloper@gmail.com>
2023-06-02 13:41:19 +0530

    create about and explore pages

---
+++ src/pages/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import { AboutSection } from "../../components/about";
+
+function About() {
+  return (
+    <>
+   <div className="setheaderRoute">
+   <Header/>
+   </div>
+  <AboutSection/>
+    </>
+
+  );
+}
+
+export default About;
---
+++ src/pages/Explore/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import { ExploreSection } from "../../components/Explore";
+
+function Explore() {
+  return (
+    <>
+   <div className="setheaderRoute">
+   <Header/>
+   </div>
+  <ExploreSection/>
+    </>
+
+  );
+}
+
+export default Explore;
